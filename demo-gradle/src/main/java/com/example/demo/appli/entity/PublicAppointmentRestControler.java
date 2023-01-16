741cfc1395f0bd597cd0d835c741081aa5665266
LiMerl <merlinlimon@gmail.com>
2023-01-16 17:31:02 +0100

    Rdv post fonctionne mais pas de valeur

--- demo-gradle/src/main/java/com/example/demo/appli/entity/PublicAppointmentRestControler.java
+++ demo-gradle/src/main/java/com/example/demo/appli/entity/PublicAppointmentRestControler.java
@@ -16,20 +16,22 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "/api/public/rendezvous")
 public class PublicAppointmentRestControler {
     
     @Autowired
     private AppointmentRepository appointmentRepository;  
-    public PublicAppointmentRestControler(AppointmentRepository appointmentRepository) {
-        this.appointmentRepository = appointmentRepository;
-    }
+    // public PublicAppointmentRestControler(AppointmentRepository appointmentRepository) {
+    //     this.appointmentRepository = appointmentRepository;
+    // }
 
     @CrossOrigin(origins = "http://localhost:4200")
-    @PostMapping(path = "/api/public/rendezvous/")
+    @PostMapping
     public ResponseEntity<String> controllerMethod( @RequestParam String date, @RequestParam String prenom, @RequestParam String center, @RequestParam String datenaissance , @RequestParam String nom  ) {
 
      /*   // System.out.println("customQuery = profesion " + customQuery.containsKey("profesion"));
@@ -37,15 +39,15 @@ public class PublicAppointmentRestControler {
         System.out.println("customQuery = passwrd " + customQuery.containsKey("password"));
         System.out.println("customQuery = pseudo " + customQuery.containsKey("login"));
  */
-        /* VaccinationCenter vacc=new VaccinationCenter(); */
-        /* Appointment appointment = new Appointment();
+         VaccinationCenter vacc=new VaccinationCenter(); 
+         Appointment appointment = new Appointment();
         appointment.setPrenom(prenom);
         appointment.setNom(nom);
         appointment.setDate(date);
         appointment.setDatenaissance(datenaissance);
-       // appointment.setCenter(center);
-        //appointment.setCenter(center);
-        appointmentRepository.save(appointment);  */
+       //appointment.setCenter(center);
+        // appointment.setCenter(center);
+        appointmentRepository.save(appointment);  
 
         return new ResponseEntity<>(HttpStatus.OK);
    
